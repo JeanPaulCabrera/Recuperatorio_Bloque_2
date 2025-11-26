69f2a0b7723942f64bce6fd74a0e578a89413862
JeanPaulCabreraFlores <126268240+lomandopuntocom@users.noreply.github.com>
2025-11-26 16:10:05 -0400

    Agregar botones

--- ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +29,8 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+                    BotonesRow()
                 }
             }
         }
@@ -43,5 +50,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Ejercicio1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun BotonesRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth() // Ocupa todo el ancho de la pantalla
+            .padding(16.dp), // Margen externo
+        horizontalArrangement = Arrangement.spacedBy(16.dp) // Espacio entre los botones
+    ) {
+        // Botón Galería (usamos weight para que ambos midan lo mismo)
+        Button(
+            onClick = { /* Acción para Galería */ },
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = "Galeria")
+        }
+
+        // Botón Llamadas
+        Button(
+            onClick = { /* Acción para Llamadas */ },
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(text = "Llamadas")
+        }
     }
 }
